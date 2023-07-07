1413adc844d414c75f0e787a0e53fa03b1809e29
Frederick Obeng-Nyarko <nernuer@gmail.com>
2023-07-07 03:06:10 +0000

    :construction: Setup Prisma and Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersController } from './orders/controllers/orders/orders.controller';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController, OrdersController],
   providers: [AppService],
 })
